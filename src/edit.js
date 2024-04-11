fb27e4b8eb6f157e50ff52f5c611069bd393dca2
Gustavo_H <gustavo.hilario@automattic.com>
2024-04-11 08:18:23 -0500

    101. Manipulating Meta Fiels Using the Data Stores

--- src/edit.js
+++ src/edit.js
@@ -1,13 +1,27 @@
+// prettier-ignore
 import { __ } from '@wordpress/i18n';
-
+import { TextControl } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 import { useBlockProps } from '@wordpress/block-editor';
-
 import './editor.scss';
 
 export default function Edit() {
+	const postType = useSelect( ( select ) =>
+		select( 'core/editor' ).getCurrentPostType()
+	);
+	//eslint-disable-next-line no-unused-vars
+	const [ meta, setMeta ] = useEntityProp( 'postType', postType, 'meta' );
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Boilerplate – hello from the editor!', 'boilerplate' ) }
-		</p>
+		<div { ...useBlockProps() }>
+			<TextControl
+				label={ __( 'Post Subtitle', 'metadata-block' ) }
+				// value= {}
+				onChange={ ( value ) => {
+					//eslint-disable-next-line no-console
+					console.log( value );
+				} }
+			/>
+		</div>
 	);
 }
